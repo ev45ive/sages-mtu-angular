dafbb973f384445fd9af381bb310be7bbffba0b3
Mateusz Kulesza <ev45ive@gmail.com>
2024-04-08 15:05:13 +0200

    Exercise: selecing playlists

--- src/app/playlists/components/playlists-list/playlists-list.component.ts
+++ src/app/playlists/components/playlists-list/playlists-list.component.ts
@@ -1,32 +1,39 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
-NgFor
+NgFor;
 
+const mockPlaylists = [
+  {
+    id: '123',
+    name: 'Playlist 123',
+    public: false,
+    description: 'Ala ma kota',
+  },
+  {
+    id: '234',
+    name: 'Playlist 234',
+    public: true,
+    description: 'super  kota',
+  },
+  {
+    id: '34',
+    name: 'Playlist 34',
+    public: false,
+    description: 'Ala ma kota',
+  },
+];
 @Component({
   selector: 'app-playlists-list',
   templateUrl: './playlists-list.component.html',
   styleUrl: './playlists-list.component.scss',
 })
 export class PlaylistsListComponent {
-  playlists = [
-    {
-      id: '123',
-      name: 'Playlist 123',
-      public: false,
-      description: 'Ala ma kota',
-    },
-    {
-      id: '234',
-      name: 'Playlist 234',
-      public: true,
-      description: 'super  kota',
-    },
-    {
-      id: '34',
-      name: 'Playlist 34',
-      public: false,
-      description: 'Ala ma kota',
-    },
-  ];
+  playlists = mockPlaylists;
+
+  selectedId = '234';
+
+  select(id: string) {
+    this.selectedId = id;
+  }
 }
