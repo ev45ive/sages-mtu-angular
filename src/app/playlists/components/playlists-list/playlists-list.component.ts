a69679833afc6316149c434bef74eac47ac8826a
Mateusz Kulesza <ev45ive@gmail.com>
2024-04-08 14:26:40 +0200

    Exercise: Rendering lists

--- src/app/playlists/components/playlists-list/playlists-list.component.ts
+++ src/app/playlists/components/playlists-list/playlists-list.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-playlists-list',
   templateUrl: './playlists-list.component.html',
-  styleUrl: './playlists-list.component.scss'
+  styleUrl: './playlists-list.component.scss',
 })
 export class PlaylistsListComponent {
-
+  playlists = [
+    {
+      id: '123',
+      name: 'Playlist 123',
+      public: false,
+      description: 'Ala ma kota',
+    },
+    {
+      id: '234',
+      name: 'Playlist 234',
+      public: true,
+      description: 'super  kota',
+    },
+    {
+      id: '34',
+      name: 'Playlist 34',
+      public: false,
+      description: 'Ala ma kota',
+    },
+  ];
 }
