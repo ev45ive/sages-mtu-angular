07a34eba4d107913f2586c5d4db645f959ec18b1
Mateusz Kulesza <ev45ive@gmail.com>
2024-04-08 10:57:18 +0200

    Exercise: Text interpolation

--- src/app/playlists/components/playlists-details/playlists-details.component.ts
+++ src/app/playlists/components/playlists-details/playlists-details.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-playlists-details',
   templateUrl: './playlists-details.component.html',
-  styleUrl: './playlists-details.component.scss'
+  styleUrl: './playlists-details.component.scss',
 })
 export class PlaylistsDetailsComponent {
-
+  playlist = {
+    id: '123',
+    name: 'Playlist 123',
+    public: true,
+    description: 'Ala ma kota',
+  };
 }
